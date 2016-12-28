70c982f73e4bed62d191e26bcede79d511f3428f
Steven Gallo <steven.gallo@outook.com>
2016-12-27 22:09:37 -0500

    1-6

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+import StorePicker from './components/storePicker'
+
+render(<StorePicker/>, document.querySelector('#main'));
