111aa8e6b9fecc1bf967bc152ae5a40184038883
Steven Gallo <steven.gallo@outook.com>
2016-12-28 00:06:45 -0500

    video 8-11

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render(){
-    return(
+
+const Header = (props) => {
+  return(
       <header className="top">
         <h1>
         Catch
@@ -12,10 +12,13 @@ class Header extends React.Component {
         </span>
         Day
         </h1>
-        <h3 className="tagline">{this.props.tagline}</h3>
+        <h3 className="tagline"><span>{props.tagline}</span></h3>
       </header>
     )
-  }
 }
+
+
+
+
 
 export default Header;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './css/style.css';
-import StorePicker from './components/storePicker';
-import App from './components/app';
+import { BrowserRouter, Match, Miss} from 'react-router';
 
-render(<App/>, document.querySelector('#main'));
+import './css/style.css';
+import App from './components/app';
+import StorePicker from './components/storePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+    <div>
+    <Match exactly pattern ="/" component ={StorePicker} />
+    <Match pattern ="store/:storeId" component ={App} />
+    <Miss component ={NotFound}/>
+    </div>
+    </BrowserRouter>
+    )
+}
+render(<Root/>, document.querySelector('#main'));
